6d77c3e40c06344a34d2511f94d2b502ddf19379
PrinceOforiAnkamah <sabastainofori@gmail.com>
2024-10-01 10:37:09 +0000

    change repository type to UUID

--- src/main/java/api/springsecurity/customerservice/repositories/UserRepository.java
+++ src/main/java/api/springsecurity/customerservice/repositories/UserRepository.java
@@ -4,15 +4,15 @@ import api.springsecurity.customerservice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
     Optional<User> findByPhone(String phone);
     Optional<User> findByUsernameOrEmailOrPhone(String username, String email, String phone);
     Optional<User> findByUsernameAndLockedIsFalse(String username);
-    Optional<User> findByUsernameAndLockedIsTrue(String username);
     Optional<User> findByPhoneAndLockedIsFalse(String phone);
     Optional<User> findByEmailAndLockedIsFalse(String email);
 }
