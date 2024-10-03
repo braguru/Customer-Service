e06aff1fd218b918212567afbfd0b7d95ab825c3
PrinceOforiAnkamah <sabastainofori@gmail.com>
2024-10-03 13:52:40 +0000

    add throws exception

--- src/main/java/api/springsecurity/customerservice/service/userprofile/UserProfileService.java
+++ src/main/java/api/springsecurity/customerservice/service/userprofile/UserProfileService.java
@@ -3,11 +3,13 @@ package api.springsecurity.customerservice.service.userprofile;
 import api.springsecurity.customerservice.dto.ProfileResponse;
 import api.springsecurity.customerservice.payload.ProfileRequest;
 
+import java.io.IOException;
+
 public interface UserProfileService {
 
     ProfileResponse getProfile();
 
-    ProfileResponse updateProfile(ProfileRequest request);
+    ProfileResponse updateProfile(ProfileRequest request) throws IOException;
 
     ProfileResponse deleteAccount();
 }
