c5083fcc4b1a4fe716d8eacf0be2a3a08dc56a0b
PrinceOforiAnkamah <sabastainofori@gmail.com>
2024-11-06 08:05:04 +0000

    add access denied handler for authorization handling

---
+++ src/main/java/api/springsecurity/customerservice/exceptions/CustomAccessDeniedHandler.java
@@ -0,0 +1,24 @@
+package api.springsecurity.customerservice.exceptions;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * This class is used to handle the AccessDeniedHandler.
+ * It is used to handle the AccessDeniedException.
+ * It is used to handle Access Denied (When a user lacks the required permission).
+ */
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_FORBIDDEN, accessDeniedException.getMessage());
+    }
+}
